e70cf2566d1f88a46a2a2ed715fa7e05540fdfbb
GARSON ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGGA5@OOCL.COM>
2019-07-17 16:06:35 +0800

    complete the function find companies by company name.  And reverse the list

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -2,7 +2,11 @@ package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
@@ -20,12 +24,6 @@ public class CompanyResource {
     public Company add(@RequestBody Company company) {
         return companyRepository.save(company);
     }
-
-  /* @PostMapping(produces = {"application/json"})
-   public Company add(@RequestBody Company company) {
-       return companyRepository.saveAndFlush(company);
-   }*/
-
     @PutMapping
     public Company update(@RequestBody Company company){
         return companyRepository.save(company);
@@ -34,6 +32,18 @@ public class CompanyResource {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         companyRepository.deleteById(id);
+    }
+
+
+    @GetMapping("/{companyName}")
+    public List<Company> findCompanyByCompanyName(@PathVariable String companyName){
+        List<Company> companies = companyRepository.findAll();
+        return companies.stream().filter(company -> company.getName().equals(companyName)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/reverse")
+    public List<Company> findCompaniesAndReverse(){
+        return companyRepository.findAll(Sort.by(Sort.Order.desc("id")));
     }
 
 }
